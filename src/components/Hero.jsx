23303cdf8454b3f946cc6db9b31ad78b4bcfaf04
Chaddddddd11 <dcc2ndyrrep.nfjpia2324@gmail.com>
2026-07-08 07:09:09 +0800

    Use full GitHub Pages URL for profile image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -75,7 +75,7 @@ const Hero = () => {
         >
           <div className="w-48 h-48 mx-auto rounded-full bg-gradient-to-br from-maroon-500 via-maroon-600 to-gold-500 p-1 shadow-2xl">
             <img 
-              src="./profile.jpg" 
+              src="https://goodyboyie2-byte.github.io/resume-2/profile.jpg" 
               alt="Richard Budlong" 
               className="w-full h-full rounded-full object-cover"
             />
